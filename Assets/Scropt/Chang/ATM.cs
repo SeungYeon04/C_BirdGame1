041f9d41f3c4dc8f0ef9b58faec321b60cb8255f
SeungYeon04 <100332811+SeungYeon04@users.noreply.github.com>
2023-12-12 17:35:23 +0900

    입금출금

--- Assets/Scropt/Chang/ATM.cs
+++ Assets/Scropt/Chang/ATM.cs
@@ -7,15 +7,16 @@ using UnityEngine.UI;
 
 public class ATM : MonoBehaviour
 {
+    //숫자 표시!!만 하는 아래 텍스트들 종류 
     public Text PlayerMoney; 
     public Text ATMMoney;
 
+    //입력 창 인풋 텍스트 받는 칸!! 
     public InputField PlaseMoney;
     public InputField MinuseMoney;
 
-    //일단 임시 로그인 정보 
-    public static int PMoney; //그냥 선언해본 돈 
-    public static int MMoney; //입금돈 
+    //은행 저장해둔 돈 값 저장소!! 데이터 
+    public static int MMoney; 
 
     public void Start()
     {
@@ -42,19 +43,11 @@ public class ATM : MonoBehaviour
     //atm저장공간 안에다 돈 더하기 InputField PlaseMoney에 입력된 만큼 
     public void MoneyPluse()
     {
+        //문자열을 int로 바꿔서 더하기 
         int MoneyATMin = int.Parse(PlaseMoney.text);
 
-        if (PlayerStack.Status.Money >= MoneyATMin) //현재돈 보다 입력값이 작다면
+        if (PlayerStack.Status.Money >= MoneyATMin) //현재돈 보다 입력 값이 작다면
         {
-            //PlaseMoney.text += int.Parse(ATMMoney.text);
-            //int ATMInput = int.Parse(ATMMoney.text); 
-            //ATMMoney.text = PlayerMoney.text;
-            //ATMInput += int.Parse(PlayerMoney.text);
-
-            Debug.Log("!");
-            //문자열을 int로 바꿔서 더하기 
-
-
             PlayerStack.Status.Money -= MoneyATMin;
             MMoney += MoneyATMin;
 
@@ -62,6 +55,8 @@ public class ATM : MonoBehaviour
             {
                 //PlayerStack.Status.Money = 100;
                 PlayerMoney.text = "보유 현금: " + PlayerStack.Status.Money.ToString();
+                 PlayerPrefs.SetInt(PlaseMoney.text, PlayerStack.Status.Money);
+                  PlayerPrefs.Save(); 
             }
             if (ATMMoney != null)
             {
@@ -71,9 +66,38 @@ public class ATM : MonoBehaviour
 
             Debug.Log(PlayerStack.Status.Money);
 
+
             //다시 밖에선 string으로 표시 
-
-
         }
+        else
+        {
+            Debug.Log("잘못된 값 입력");
+        }
+    }
+
+    public void MoneyMinus()
+    {
+        int MoneyATMout = int.Parse(MinuseMoney.text); //text를 잠시 int형 데이터로 만들어서 
+
+        if(MMoney >= MoneyATMout) //은행 돈보디 입력 값이 작다면 
+        {
+            PlayerStack.Status.Money += MoneyATMout; 
+            MMoney -= MoneyATMout; 
+
+            if(MinuseMoney != null)
+            {
+                PlayerMoney.text = "보유 현금 : " + PlayerStack.Status.Money.ToString(); 
+
+            }
+            if(PlayerMoney != null)
+            {
+                ATMMoney.text = "저금 금액 : " + MMoney.ToString(); 
+                //PlayerPrefs.SetInt(MinuseMoney.text, MoneyATMout);
+                //PlayerPrefs.Save();
+
+            }
+        }       
+
+
     }
 }
